a13d17c95d78fd7f7acdfa7688c7bea80edb7a01
Gabriela Mancini <gabrielamancini@Gabrielas-MacBook-Pro.local>
2021-02-06 19:28:43 -0300

    Portfolio effects and JS

--- js/script.js
+++ js/script.js
@@ -1,16 +1,12 @@
 let overLay = ()=>{
-    let portfolio = document.querySelectorAll(".gallery__item"); 
+    let portfolio = document.querySelectorAll(".gallery__item a"); 
+  
    
-    for(let i = 0; i < portfolio.length; i++){ 
-        let overlay = document.createElement("div");
-        let overlayP = document.createElement("p");
-        overlay.className ="overlay"; 
-
-        let title = portfolio[i].getAttribute("title");
-        overlayP.className = "title";
-        overlayP.innerHTML = title;
-        overlay.appendChild(overlayP);
-        portfolio[i].appendChild(overlay); 
+    for(let i = 0; i < portfolio.length; i++){  
+        let overlaySpan = document.createElement("span"); 
+        overlaySpan.className = "title";
+        overlaySpan.innerHTML = "Visit Site"; 
+        portfolio[i].appendChild(overlaySpan); 
     }; 
     
 }
